f92409571ef1786477967ab3b987664f35488dd0
AllanGaBRs <allanmediogames@gmail.com>
2025-12-10 23:06:08 -0300

    feat: delete product test

---
+++ src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dscatalog.repositories;
+
+import com.devsuperior.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void deleteShouldDeleteObjectWhenExists(){
+
+        long existingId = 1L;
+
+        productRepository.deleteById(existingId);
+
+        Optional<Product> product = productRepository.findById(existingId);
+        Assertions.assertFalse(product.isPresent());
+    }
+
+}
